413f1983f641998a92f40b78e133a859469fdc89
shweta madhale <madhaleshwetar@gmail.com>
2025-07-13 22:01:32 -0700

    Connected Dashboard to Firebase Firestore

--- groceriq/src/firebase.js
+++ groceriq/src/firebase.js
@@ -1,15 +1,17 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsQL5l-pNx_hz4H-J7RY81tGl5qKpiLqY",
-  authDomain: "groceriq.firebaseapp.com",
-  projectId: "groceriq",
-  storageBucket: "groceriq.firebasestorage.app",
-  messagingSenderId: "495413107202",
-  appId: "1:495413107202:web:f7964c6e83cb5e0fb9651b"
+  // ✅ Paste your actual config from Firebase Console here
+    apiKey: "AIzaSyDsQL5l-pNx_hz4H-J7RY81tGl5qKpiLqY",
+    authDomain: "groceriq.firebaseapp.com",
+    projectId: "groceriq",
+    storageBucket: "groceriq.firebasestorage.app",
+    messagingSenderId: "495413107202",
+    appId: "1:495413107202:web:f7964c6e83cb5e0fb9651b"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
