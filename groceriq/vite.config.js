53ed6d5d775941b4d3888150a059ae113defb37b
shweta madhale <madhaleshwetar@gmail.com>
2025-08-25 09:35:11 -0700

    Fixed Vite Config

--- groceriq/vite.config.js
+++ groceriq/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-dotenv.config(); // ⬅ add this line
-
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    },
+    server: {
+      port: 3000,
+    },
+  };
 });
