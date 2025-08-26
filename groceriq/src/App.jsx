f6811cd4f0a67fee0596c182677c00a673d2cf96
shweta madhale <madhaleshwetar@gmail.com>
2025-08-25 19:56:32 -0700

    Updated routing

--- groceriq/src/App.jsx
+++ groceriq/src/App.jsx
@@ -1,6 +1,8 @@
+// App.jsx
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Auth from "./pages/Auth";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Onboarding from "./pages/Onboarding";
 import Suggest from "./pages/Suggest";
@@ -12,7 +14,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/auth" element={<Auth />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       
       <Route path="/dashboard" element={
         <ProtectedRoute>
@@ -43,7 +46,6 @@ const App = () => {
           <Profile />
         </ProtectedRoute>
       } />
-      
     </Routes>
   );
 };
